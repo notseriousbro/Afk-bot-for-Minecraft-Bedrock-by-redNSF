d8a71b3ec5d32e6acf3bbea3159bb348a7f3b2e7
notseriousbro <prakhartiwari16@outlook.com>
2026-04-29 12:54:43 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,5 +3,8 @@ const client = bedrock.createClient({
   host: 'DOMIN4TORSMP.aternos.me',   
   port: 52442,         // optional, default 19132
   username: 'SERVERTERMINAL',   // the username you want to join as, optional if online mode
-  offline: true       // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
-})
+  offline: true  // optional, default false. if true, do not login with Xbox Live. You will not be asked to sign-in if set to true.
+  client.on('error', (err) => {
+  console.error('Connection error:', err.message);
+  setTimeout(connectToServer, 5000); // Retry after 5 seconds
+});   
